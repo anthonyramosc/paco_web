dffa3e794b6723b086f709a0d8477ed78401b859
AracnoGrpup <fgorellana@visitaecuador.com>
2025-04-16 14:49:39 -0500

    tiktok

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,23 +6,13 @@ import TikTokSlider from '../components/titktok.tsx';
 const Dashboard: FC = () => {
 
 
-        const videos = [
-            {
-                id: '1',
-                videoUrl: 'https://www.tiktok.com/@pacoelmorlaco99/video/7353728401980673286',
-                username: 'usuario1',
-                caption: '¡Mi primer TikTok! #viral',
-                likes: 1200,
-                comments: 45,
-                shares: 23
-            },
-        ];
 
     return (
     <div className="flex flex-col align-top">
        <HeroBanner/>
         <ProfileComponent/>
-        <TikTokSlider videos={videos} />
+        <TikTokSlider  />
+
     </div>
     );
 }
